9666099045fa35ed5d33150da290e37db579d35c
Ubuntu <ubuntu@groupone>
2016-12-01 18:29:24 +0000

    Message4

--- Gemfile
+++ Gemfile
@@ -4,13 +4,16 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 
 gem 'haml-rails'
 gem 'better_errors'
 gem 'binding_of_caller'
+
+gem 'pg', '~> 0.18'
+gem 'activerecord-reset-pk-sequence'
 
 gem 'font-awesome-sass'
 gem 'font-awesome-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'main/index'
 
   get 'main/help'
@@ -10,9 +11,9 @@ Rails.application.routes.draw do
   root 'main#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match 'index',    to: 'main#index',        via: 'get'
+  match 'about',    to: 'main#about',        via: 'get'
+  match 'help',    to: 'main#help',        via: 'get'
+  match 'contacts',    to: 'main#contacts',        via: 'get'
 end
 
-match 'index',    to: 'main#index',        via: 'get'
-match 'about',    to: 'main#about',        via: 'get'
-match 'help',    to: 'main#help',        via: 'get'
-match 'contacts',    to: 'main#contacts',        via: 'get'
