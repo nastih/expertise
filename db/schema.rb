e5e76ab29a2224d9a06e15f40aa2de3020f48cbf
Ubuntu <ubuntu@groupone>
2016-12-08 18:08:28 +0000

    Message 08.12.2016

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206162013) do
+ActiveRecord::Schema.define(version: 20161208175504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161206162013) do
     t.float    "ave_value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "theme_id"
   end
 
   create_table "themes", force: :cascade do |t|
